a5040da5cdbc91a45663e578f82eba9a9e198fdb
Oscar Varela <oscar.varela@tilatina.com>
2025-06-14 09:30:59 -0600

    Add endpoints

--- src/route/orderRouter.ts
+++ src/route/orderRouter.ts
@@ -18,7 +18,7 @@ router.patch('/:id', updateOrder);
 router.delete('/:id', deleteOrder);
 router.get('/table/:numberTable', getOrdersByTable);
 router.get('/status/:status', getOrdersByStatus);
-router.get('/date/:date', getOrdersByDate);
+router.get('/date', getOrdersByDate);
 router.get('/:id', getOrderById);
 
 export default router;
